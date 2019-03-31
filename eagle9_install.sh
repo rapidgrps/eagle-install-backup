26449ffc3f3248859d04f597d9bdb9b6822d5bb3
ShaheenHossain <rapidgrps@princegroup-bd.com>
2019-03-31 14:10:14 +0600

    Update eagle9_install.sh

--- eagle9_install.sh
+++ eagle9_install.sh
@@ -16,14 +16,14 @@
  
 ##fixed parameters
 #eagle
-OE_USER="eagle"
+OE_USER="eagle0959"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 #The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 #Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 #Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8090"
+OE_PORT="8059"
 #Choose the Odoo version which you want to install. For example: 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 9.0
 OE_VERSION="9.0"
@@ -94,7 +94,7 @@ else
 fi
 	
 echo -e "\n---- Create EAGLE system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE0959' --group $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
 
