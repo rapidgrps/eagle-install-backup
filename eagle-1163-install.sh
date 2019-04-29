794ff5320bde1a07b89cc0216fef26f237f23ae5
ShaheenHossain <rapidgrps@princegroup-bd.com>
2019-04-29 10:53:52 +0600

    Update eagle-1163-install.sh

--- eagle-1163-install.sh
+++ eagle-1163-install.sh
@@ -67,10 +67,12 @@ echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install python3 python3-pip
 
 echo -e "\n---- Install tool packages ----"
-sudo apt-get install wget git bzr python-pip gdebi-core -y
+#sudo apt-get install wget git bzr python-pip gdebi-core -y
+
+sudo apt-get install wget git bzr python-pip gdebi-core libpcap-dev libpq-dev -y
 
 echo -e "\n---- Install python packages ----"
-sudo apt-get install python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 libpq-dev python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil -y
+sudo apt-get install python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 libpq-dev python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-decorator python-requests python-passlib python-pil -y
 sudo pip3 install pandas pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2 libpq-dev psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd 
 
 echo -e "\n---- Install python libraries ----"
